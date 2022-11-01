5775bc8172e6fd62b4fe15926ea6133a5a972866
Safat Mobid <safatmobid@gmail.com>
2022-11-01 09:10:03 -0400

    Completed loops exercise

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,7 @@
+let value = "I hate loops";
+let l;
+for (l = 0; l < 7; l++) 
+{
+ value="I love Loops"
+ console.log(value);
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,9 @@
+let text="while sucks";
+let i=0;
+
+while (i < 7)
+{
+    text="I love loops"
+    console.log(text);
+    i++;
+}
